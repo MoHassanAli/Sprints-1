ca24ccf7feff638c60ac1151edaa75d2db833d80
MoHassanAli <86575412+MoHassanAli@users.noreply.github.com>
2021-08-01 18:11:27 +0200

    Update UART_int.h

--- EmbeddedSysLevel2/ATM_Project/ATM/ATM/MCAL/UART/UART_int.h
+++ EmbeddedSysLevel2/ATM_Project/ATM/ATM/MCAL/UART/UART_int.h
@@ -22,78 +22,14 @@
 
 
 
-/**
- * @brief Initialized UART 
- * 
- * @return EN_ERRORSTATE_t E_OK | E_ERROR
- */
 EN_ERRORSTATE_t UART_ENInit(void);
-
-
-/**
- * @brief Send One Character
- * 
- * @param Data char to send
- * @return EN_ERRORSTATE_t E_OK | E_ERROR
- */
 EN_ERRORSTATE_t UART_ENSendData(uint8_t Data);
-
-
-/**
- * @brief Send Without Polling On Data Register Flag 
- * 
- * @param Data char to send
- * @return EN_ERRORSTATE_t E_OK | E_ERROR
- */
 EN_ERRORSTATE_t UART_ENSendNoBlock(uint8_t Data);
-
-
-/**
- * @brief Receive Data From another device
- * 
- * @return uint8_t Data From Register
- */
 uint8_t UART_u8ReceiveData(void);
-
-
-/**
- * @brief Receive Data without Polling
- * 
- * @param Data 
- * @return uint8_t Data From Register
- */
 uint8_t UART_u8ReceiveNoBlock(uint8_t *Data);
-
-
-
-/**
- * @brief Enable Transmit Interrupt
- * 
- */
 void UART_voidTXInterruptEnable(void);
-
-
-
-/**
- * @brief Enable Receive Interrupt
- * 
- */
 void UART_voidRXInterruptEnable(void);
-
-
-/**
- * @brief Asynchronous Function Send 
- * 
- * @param str Data to send
- */
 void UART_voidSendString_Ashync(uint8_t *str);
-
-
-/**
- * @brief Asynchronous Function Receive 
- * 
- * @param Str Data to Receive
- */
 void UART_voidReceiveString_Ashync(uint8_t *Str);
 
 
